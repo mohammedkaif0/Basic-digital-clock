7b0170135d0100f39d81d4f81740d632833eef28
mohammed kaif <mohammedkaifak435@gmail.com>
2021-06-20 22:23:35 +0530

    Initial commit

--- index.js
+++ index.js
@@ -4,15 +4,10 @@ const Time = new Date();
 let hours = Time.getHours();
 let minutes = Time.getMinutes();
 let seconds = Time.getSeconds();
-let am_pm="AM";
-if(hours < 12){
-    hours-=12;
-    am_pm="PM";
-}
-if(hours===0){
-    hours =12;
-    hours="AM";
-}
+let am_pm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
 const currentTime = hours +":"+ minutes +":"+ seconds + " "+ am_pm;
 document.querySelector('#clock').innerHTML=currentTime;
 }
